bbf144990a6b9df3cde0b45d3abff5d82e3c3d25
yuta <inagaki3107@gmail.com>
2024-06-17 10:26:54 +0900

    code

--- My project/Assets/Scripts/HammerManager.cs
+++ My project/Assets/Scripts/HammerManager.cs
@@ -5,17 +5,32 @@ using UnityEngine;
 public class HammerManager : MonoBehaviour
 {
     public GameObject hammerObject;
-
+    Collider2D hammerCollider;
     Animator animator;
+    public GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        hammerCollider = GetComponent<Collider2D>();
+        HideColliderHammer();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if (gameManager.resultPanal.activeSelf == false)
+        {
+            ShowHammer();
+            if (Input.GetMouseButtonDown(0))
+            {
+                animator.SetTrigger("Move0");
+            }
+        }
+    }
+
+    void ShowHammer()
     {
         Vector3 mousePosition = Input.mousePosition;
         mousePosition.z = 10;
@@ -25,12 +40,20 @@ public class HammerManager : MonoBehaviour
         pos.x += (float)242;
         pos.y += (float)181.4;
         hammerObject.transform.position = pos;
+    }
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            animator.SetTrigger("Move0");
-        }
+    //アニメーションイベントで使用（手順⑤で解説）
+    public void HideColliderHammer()
+    {
+        hammerCollider.enabled = false;
+        Debug.Log("ハンマーコライダーが消えた");
+    }
 
+    //アニメーションイベントで使用（手順⑤で解説）
+    public void ShowColliderHammer()
+    {
+        hammerCollider.enabled = true;
+        Debug.Log("ハンマーコライダーがついた");
     }
 
 }
