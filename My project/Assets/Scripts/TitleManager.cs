e5299e7269bf1cad44ff9fdc6380abcbbee8778b
yuta <inagaki3107@gmail.com>
2024-06-17 12:14:41 +0900

    a

--- My project/Assets/Scripts/TitleManager.cs
+++ My project/Assets/Scripts/TitleManager.cs
@@ -9,8 +9,8 @@ public class TitleManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
-        audioSource.Play();
+        //audioSource = GetComponent<AudioSource>();
+        //audioSource.Play();
     }
 
     public void OnStartButton()
