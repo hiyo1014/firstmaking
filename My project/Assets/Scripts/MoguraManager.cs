3320f5c01736fe541bd520012e25f6ec54f903f2
yuta <inagaki3107@gmail.com>
2024-06-17 10:53:47 +0900

    code

--- My project/Assets/Scripts/MoguraManager.cs
+++ My project/Assets/Scripts/MoguraManager.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class MoguraManager : MonoBehaviour
 {
-    //モグラのコライダーを宣言 当たり判定に使う
     Collider2D moguraCollider;
-    //アニメーターを宣言 アニメーション関係の関数実行に使う
     Animator animator;
-    //オーディオソースを宣言
+    GameManager gameManager; //追記
     AudioSource audioSource;
     public AudioClip pikopikoSE;
 
@@ -16,6 +14,7 @@ public class MoguraManager : MonoBehaviour
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>(); //追記
         animator = GetComponent<Animator>();
         moguraCollider = GetComponent<Collider2D>();
         HideColliderMogura();
@@ -26,32 +25,29 @@ public class MoguraManager : MonoBehaviour
     {
         transform.position -= new Vector3(0, 0.1f, 0);
     }
-    //アニメーションイベントで使用（手順⑤で解説）
     public void HideColliderMogura()
     {
         moguraCollider.enabled = false;
         Debug.Log("モグラコライダーが消えた");
     }
-    //アニメーションイベントで使用（手順⑤で解説）
     public void ShowColliderMogura()
     {
         moguraCollider.enabled = true;
         Debug.Log("モグラコライダーがついた");
     }
 
-    //
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //次やること、攻撃を受けたら、得点が追加される
-        //Hurtと名付けたパラメータがここで活きる
         animator.SetTrigger("Hurt");
         audioSource.PlayOneShot(pikopikoSE);
+        gameManager.AddScore(); //追記
         HideColliderMogura();
         StartCoroutine(Damage());
     }
-    //ダメージを受ける時の関数
     IEnumerator Damage()
     {
+
         yield return new WaitForSeconds(0.5f);
         animator.enabled = false;
         yield return new WaitForSeconds(1.0f);
